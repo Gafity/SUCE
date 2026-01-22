f8e54935edc77c17beadd79f86e0d51992c9bfca
Gabriel Gama <devgabrielgama@gmail.com>
2026-01-22 14:47:19 -0300

    feat[SUCE-21]: add delete and update user endpoints

--- app/lib/app/users.ex
+++ app/lib/app/users.ex
@@ -8,4 +8,16 @@ defmodule AppWeb.Users do
     |> User.changeset(attrs)
     |> Repo.insert()
   end
+
+  def get_user!(id), do: Repo.get!(User, id)
+
+  def update_user(%User{} = user, attrs) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_user(%User{} = user) do
+    Repo.delete(user)
+  end
 end
